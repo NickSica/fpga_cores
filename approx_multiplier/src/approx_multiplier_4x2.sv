f5b44429440d7aa40e738935e85b002c8f75b4b1
NickSica <njs82@drexel.edu>
2020-02-28 18:40:08 -0500

    Adds 4x2 approx multiplier and fixes 4x4 a bit.

---
+++ approx_multiplier/src/approx_multiplier_4x2.sv
@@ -0,0 +1,66 @@
+module approx_multiplier_4x2 (
+    input logic [3:0]  a_i;
+    input logic [1:0]  b_i;
+    output logic [5:0] prod_o;
+);
+
+    // P1 and P2
+    lut6_2 #(
+	     .INIT(16'hB4CCF00066AACC00)
+	     )
+    lut_inst1
+      (
+    .I0(a[0]),
+    .I1(a[1]),
+    .I2(a[2]),
+    .I3(b[0]),
+    .I4(b[1]),
+    .I5('1'),
+    .O6(prod[1]),
+    .O6(prod[2])
+);
+
+lut6_2 // P3
+#(.INIT(16'hC738F0F0FF000000))
+    lut_inst2
+(
+    .I0(a[0]),
+    .I1(a[1]),
+    .I2(a[2]),
+    .I3(a[3]),
+    .I4(b[0]),
+    .I5(b[1]),
+    .O6(prod[3])
+);
+
+// P4
+lut6_2
+#(.INIT(16'h07C0FF0000000000))
+    lut_inst3
+(
+    .I0(a[0]),
+    .I1(a[1]),
+    .I2(a[2]),
+    .I3(a[3]),
+    .I4(b[0]),
+    .I5(b[1]),
+    .O6(prod[4])
+);
+
+    // P5
+    lut6_2 #(
+    .INIT(16'hF800000000000000)
+)
+    lut_inst4 (
+    .I0(a[0]),
+    .I1(a[1]),
+    .I2(a[2]),
+    .I3(a[3]),
+    .I4(b[0]),
+    .I5(b[1]),
+    .O6(prod[5])
+);
+
+prod[0] <= '0';
+
+endmodule : approx_multiplier_4x2;
--- approx_multiplier/src/approx_multiplier_4x4.sv
+++ approx_multiplier/src/approx_multiplier_4x4.sv
@@ -1,172 +1,108 @@
+// Not my code - moved from the VHDL provided in the research paper
+// Area-Optimized Low-Latency Approximate Multipliers for FPGA-Based Hardware Accelerators
+
 module approx_multiplier_4x4 (
-	input logic       clk_i,
-	input logic [3:0] a_i,
-	input logic [3:0] b_i,
-	output logic [7:0] product_o
+    input logic        clk_i;
+    input logic [3:0]  a_i;
+    input logic [3:0]  b_i;
+    output logic [7:0] product_o;
 );
-    localparam logic [63:0] C_INIT_VALUES [0:6] = '{ 64'hB4CCF00066AACC00, 
-					      64'hC738F0F0FF000000, 
-					      64'h07C0FF0000000000, 
-					      64'hF800000000000000, 
-					      64'h5FA05FA088888888, 
-					      64'h007F7F80FF808000,
-					      64'h6666666688888880 };
-    logic 		    prod_0;
-    logic 		    prod_2;
-    logic [5:0] 	    partial_prod_0;
-    logic [5:0] 	    partial_prod_1;
-    logic [3:0] 	    carry_gen;
-    logic [3:0] 	    prop;
-   
-    LUT6_2 #(.INIT(C_INIT_VALUES[0])) lut0 (
-	    .I0(a_i[0]),
-	    .I1(a_i[1]),
-	    .I2(a_i[2]),
-	    .I3(b_i[0]),
-	    .I4(b_i[1]),
-	    .I5(1'b1),
-	    .O5(partial_prod_0[1]),
-	    .O6(partial_prod_0[2])
-    );
+    logic [5:0]        partial_prod0_x, partial_prod1_x;
+    logic [3:0]        gen0_x, prop0_x, carries0_x, out0_x;
+    logic [3:0]        gen1_x, prop1_x, carries1_x, out1_x;
+    logic [1:0]	       prod1_x;
 
-    LUT6   #(.INIT(C_INIT_VALUES[1])) lut1 (
-	    .I0(a_i[0]),
-	    .I1(a_i[1]),
-	    .I2(a_i[2]),
-	    .I3(a_i[3]),
-	    .I4(b_i[0]),
-	    .I5(b_i[1]),
-	    .O(partial_prod_0[3])
-    );
-
-    LUT6   #(.INIT(C_INIT_VALUES[2])) lut2 (
-	    .I0(a_i[0]),
-	    .I1(a_i[1]),
-	    .I2(a_i[2]),
-	    .I3(a_i[3]),
-	    .I4(b_i[0]),
-	    .I5(b_i[1]),
-	    .O(partial_prod_0[4])
-    );
-
-    LUT6   #(.INIT(C_INIT_VALUES[3])) lut3 ( 
-	    .I0(a_i[0]),
-	    .I1(a_i[1]),
-	    .I2(a_i[2]),
-	    .I3(a_i[3]),
-	    .I4(b_i[0]),
-	    .I5(b_i[1]),
-	    .O(partial_prod_0[5])
-    );
-
-    LUT6_2 #(.INIT(C_INIT_VALUES[0])) lut4 (
-	    .I0(a_i[0]),
-	    .I1(a_i[1]),
-	    .I2(a_i[2]),
-	    .I3(b_i[2]),
-	    .I4(b_i[3]),
-	    .I5(1'b1),
-	    .O5(partial_prod_1[1]),
-	    .O6(partial_prod_1[2])
-    );
+    prop0_x[3] <= partial_prod1_x[4];
+    gen0_x[3] <= partial_prod1_x[5];
+    prod[6:3] <= out0_x[3:0];
+    prod_o[7] <= carries1[3];
+    prod_o[1] <= partial_prod0_x[1];
     
-    LUT6   #(.INIT(C_INIT_VALUES[1])) lut5 ( 
-	    .I0(a_i[0]),
-	    .I1(a_i[1]),
-	    .I2(a_i[2]),
-	    .I3(a_i[3]),
-	    .I4(b_i[2]),
-	    .I5(b_i[3]),
-	    .O(partial_prod_1[3])
+    approx_multiplier_4x2 lut1 (
+        .a_i,
+	.b_i(b_i[1:0]),
+	.prod(partial_prod0_x)
     );
 
-    LUT6   #(.INIT(C_INIT_VALUES[3])) lut6 (
-	    .I0(a_i[0]),
-	    .I1(a_i[1]),
-	    .I2(a_i[2]),
-	    .I3(a_i[3]),
-	    .I4(b_i[2]),
-	    .I5(b_i[3]),
-	    .O(carry_gen[3])
+    // partial_prod_1[4] is tied to prop0_x
+    // partial_prod_1[5] is tied to gen3
+    approx_multiplier_4x2 lut2 (
+        .a_i,
+	.b_i(b_i[3:2]),
+	.prod(partial_prod1_x)
     );
 
-    LUT6_2 #(.INIT(C_INIT_VALUES[4])) lut7 (
-	    .I0(a_i[0]),
-	    .I1(b_i[0]),
-	    .I2(b_i[2]),
-	    .I3(partial_prod_0[2]),
-	    .I4(1'b1),
-	    .I5(1'b1),
-	    .O5(prod_0),
-	    .O6(prod_2)
+    lut6_2 #(
+        .INIT(16'h007F7F80FFEAEA00)
+    ) 
+    lut_inst10 (  // P5
+.I0(partial_prod0_x[2]),
+.I1(a[0]),
+.I2(b[2]),
+.I3(partial_prod0_x[3]),
+.I4(partial_prod1_x[1]),
+.I5(1'b1),
+.O5(gen0_x[0]),
+.O6(prop0_x[0])        
     );
 
-    LUT6_2 #(.INIT(C_INIT_VALUES[5])) lut8 (
-	    .I0(partial_prod_0[2]),
-	    .I1(a_i[0]),
-	    .I2(b_i[2]),
-	    .I3(partial_prod_0[3]),
-	    .I4(partial_prod_1[1]),
-	    .I5(1'b1),
-	    .O5(carry_gen[0]),
-	    .O6(prop[0])
-    );
-
-    LUT6_2 #(.INIT(C_INIT_VALUES[6])) lut9 (
-	    .I0(partial_prod_0[4]),
-	    .I1(partial_prod_1[2]),
-	    .I2(1'b1),
-	    .I3(1'b1),
-	    .I4(1'b1),
-	    .I5(1'b1), 
-  	    .O5(carry_gen[1]),
-	    .O6(prop[1])
-    );
-
-    LUT6_2 #(.INIT(C_INIT_VALUES[6])) lut10 (
-	    .I0(partial_prod_0[5]),
-	    .I1(partial_prod_1[3]),
-	    .I2(1'b1),
-	    .I3(1'b1),
-	    .I4(1'b1),
-	    .I5(1'b1),
-	    .O5(carry_gen[2]),
-	    .O6(prop[2])
-    );
-
-    LUT6   #(.INIT(C_INIT_VALUES[2])) lut11 (
-	    .I0(a_i[0]),
-	    .I1(a_i[1]),
-	    .I2(a_i[2]),
-	    .I3(a_i[3]),
-	    .I4(b_i[2]),
-	    .I5(b_i[3]),
-	    .O(prop[3])
-    );
-
-    always_ff @(posedge clk_i)
-    p_assign_products : begin
-	product_o[0] <= prod_0;
-	product_o[1] <= partial_prod_0[1];
-	product_o[2] <= prod_2;	
+    lut6_2 #(
+	 .INIT(16'h6666666688888888)
+	     )
+lut_inst11 (
+	.I0(partial_prod0_x[4]),
+	.I1(partial_prod1_x[2]),
+	.I2(1'b1),
+	.I3(1'b1),
+	.I4(1'b1),
+	.I5(1'b1),
+	.O5(gen0_x[1]),
+	.O6(prop0_x[1])
+	);
 	
-    	// used to be i = 2
-        for(int i = 3; i <= 5; i++) begin 
-	    product_o[i] <= partial_prod_0[i] + partial_prod_1[i - 2];
-        end
-	
-	for(int i = 6; i <= 7; i++) begin
-            product_o[i] <= partial_prod_1[i - 2];
-	end
-    end
+lut6_2 #(
+       .INIT(16'h6666666688888888)
+)
+lut_inst12 (
+	.I0(partial_prod0_x[5]),
+	.I1(partial_prod1_x[3]),
+	.I2(1'b1),
+	.I3(1'b1),
+	.I4(1'b1),
+	.I5(1'b1),
+	.O5(gen0_x[2]),
+	.O6(prop0_x[2])
+	);
 
+
+carry4 carry_inst1 (
+	.DI(gen0_x),
+	.S(prop0_x),
+	.O(out_0_x),
+	.CO(carries1_x),
+	.CI(1'b0),
+	.CYINIT(1'b0)
+	);
+
+   prod_o[6:3] <= out0_x[3:0];
+   prod_o[7]   <= carries1_x[3];
+   prod_o[1]   <= partial_prod0_x[1];
+
+  // P1 and P2
+  lut6_2 #(
+      .INIT(16'h5FA05FA088888888)
+  ) 
+   lut_inst14 (
+	       .I0(a_i[0]),
+	       .I1(b_i[0]),
+	       .I2(b_i[2]),
+	       .I3(partial_prod0_x[2]),
+	       .I4(1'b1),
+	       .I5(1'b1),
+	       .O5(prod_o[0]),
+	       .O6(prod_o[2])
+
+
+		);
+        
 endmodule: approx_multiplier_4x4
-
-
-
-
-
-
-
-
-
